f99f6c8ec174cd62b0a759c823426a08da87345a
jeffubayi <jeffubayi@gmail.com>
2021-12-18 21:54:05 +0300

    update date component

--- pages/scores.js
+++ pages/scores.js
@@ -13,6 +13,9 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import {DatePicker,LocalizationProvider } from "@mui/lab";
+import DateAdapter from '@mui/lab/AdapterMoment';
+import TextField from '@mui/material/TextField';
 
 const axios = require("axios");
 
@@ -74,6 +77,13 @@ const paragraphStyles = css`
 
 export default function Scores(props) {
   const [scores, setScores] = useState(props.yestScoresArray);
+  const [value, setValue] = useState(null);
+  let today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  const yyyy = today.getFullYear();
+  
+  today = mm + '/' + dd + '/' + yyyy;
 
   const yesterday = new Date(new Date().valueOf() - 1000 * 60 * 60 * 24)
     .toISOString()
@@ -129,31 +139,67 @@ export default function Scores(props) {
           Game scores on the day:
           {"  "}
           {"  "}
-          <input
-            type="date"
-            onChange={(e) => {
-              const newDate = e.target.value;
-              const newDateWithoutDashes = newDate.replace(/-/g, "");
-              setDate(newDateWithoutDashes);
+          <TextField
+        id="date"
+        label="Select Game Night"
+        onChange={(e) => {
+          const newDate = e.target.value;
+          const newDateWithoutDashes = newDate.replace(/-/g, "");
+          setDate(newDateWithoutDashes);
 
-              const options = {
-                method: "GET",
-                url: `https://data.nba.net/10s/prod/v2/${newDateWithoutDashes}/scoreboard.json`,
-                params: {},
-                headers: {},
-              };
-              axios
-                .request(options)
-                .then(function (response) {
-                  const scoresArray = response.data.games;
+          const options = {
+            method: "GET",
+            url: `https://data.nba.net/10s/prod/v2/${newDateWithoutDashes}/scoreboard.json`,
+            params: {},
+            headers: {},
+          };
+          axios
+            .request(options)
+            .then(function (response) {
+              const scoresArray = response.data.games;
 
-                  return setScores(scoresArray);
-                })
-                .catch(function (error) {
-                  console.error(error);
-                });
-            }}
-          />
+              return setScores(scoresArray);
+            })
+            .catch(function (error) {
+              console.error(error);
+            });
+        }}
+        type="date"
+        defaultValue={today}
+        sx={{ width: 220 }}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+          {/* <LocalizationProvider dateAdapter={DateAdapter}>
+            <DatePicker
+              label="Select Game Night"
+              value={value}
+              onChange={(value) => {
+                const newDate = value;
+                const newDateWithoutDashes = newDate;
+                setDate(newDateWithoutDashes);
+  
+                const options = {
+                  method: "GET",
+                  url: `https://data.nba.net/10s/prod/v2/${newDateWithoutDashes}/scoreboard.json`,
+                  params: {},
+                  headers: {},
+                };
+                axios
+                  .request(options)
+                  .then(function (response) {
+                    const scoresArray = response.data.games;
+  
+                    return setScores(scoresArray);
+                  })
+                  .catch(function (error) {
+                    console.error(error);
+                  });
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+           </LocalizationProvider> */}
         </p>
 
         <div css={scoresStyles}>
@@ -169,11 +215,14 @@ export default function Scores(props) {
                 // because Carousel adds another li tag
                 // by itself. If we set this tag to li,
                 // it would cause the conflict.
-                <Link href={`/${game.gameId}`} style={{
-                  padding: "0.4rem",
-                  cursor:"pointer"
-                }}>
-                  <Card key={game.gameId}  >
+                <Link
+                  href={`/${game.gameId}`}
+                  style={{
+                    padding: "0.4rem",
+                    cursor: "pointer",
+                  }}
+                >
+                  <Card key={game.gameId}>
                     <div
                       style={{
                         justifyContent: "space-evenly",
