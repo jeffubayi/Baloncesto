c9989d7e44b28b3b5071a816ed0a06cf88ae423a
jeffubayi <jeffubayi@gmail.com>
2022-01-02 14:41:07 +0300

    add carousel

---
+++ pages/TrendingSlider.js
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+  {
+    label: 'San Francisco – Oakland Bay Bridge, United States',
+    imgPath:
+      'https://ichef.bbci.co.uk/onesport/cps/976/cpsprodpb/18462/production/_122562499_whatsubject.jpg',
+  },
+  {
+    label: 'Bird',
+    imgPath:
+      'https://ichef.bbci.co.uk/onesport/cps/976/cpsprodpb/18462/production/_122562499_whatsubject.jpg',
+  },
+  {
+    label: 'Bali, Indonesia',
+    imgPath:
+      'https://ichef.bbci.co.uk/onesport/cps/976/cpsprodpb/18462/production/_122562499_whatsubject.jpg',
+  },
+  
+];
+
+function TrendingSlider() {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = images.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {images.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  height: 405,
+                  display: 'block',
+                  overflow: 'hidden',
+                  width: '100%',
+                }}
+                src={step.imgPath}
+                alt={step.label}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+      />
+    </Box>
+  );
+}
+
+export default TrendingSlider;
