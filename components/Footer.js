dc15da5c37becc3d4bb4e66a5708c8e36aca70fa
jeffubayi <jeffubayi@gmail.com>
2022-01-06 22:10:16 +0300

    update footer color

--- components/Footer.js
+++ components/Footer.js
@@ -26,10 +26,7 @@ export default function StickyFooter() {
           py: 3,
           px: 2,
           mt: 'auto',
-          backgroundColor: (theme) =>
-            theme.palette.mode === 'light'
-              ? theme.palette.grey[200]
-              : theme.palette.grey[800],
+          backgroundColor:" #FFFF"
         }}
       >
         <Container maxWidth="sm">
