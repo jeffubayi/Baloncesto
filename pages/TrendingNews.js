904a8a0de0700693751912a09a553a565486dca5
jeffubayi <jeffubayi@gmail.com>
2022-01-10 07:45:11 +0300

    update player stats

--- pages/TrendingNews.js
+++ pages/TrendingNews.js
@@ -1,27 +1,76 @@
-import * as React from "react";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import Avatar from "@mui/material/Avatar";
-import ImageIcon from "@mui/icons-material/Image";
-import WorkIcon from "@mui/icons-material/Work";
-import BeachAccessIcon from "@mui/icons-material/BeachAccess";
-import Divider from "@mui/material/Divider";
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
 
-export default function TrendingNews() {
+export default function PinnedSubheaderList() {
+  const [stats, setStats] = React.useState([]);
+  const axios = require("axios");
+
+  async function getStats() {
+    axios
+      .get("http://stats-prod.nba.com")
+      .then((response) => {
+        const standArray = response.data;
+        console.log(`stats==`, standArray);
+        setStats(standArray);
+      });
+  }
+
+  React.useEffect(() => getStats(), []);
+
+  function getHeading(sectionId) {
+    switch (sectionId) {
+      case 0:
+        return "Points Per Game";
+        break;
+      case 1:
+        return  "Rebounds Per Game";
+        break;
+      case 2:
+        return  "Assists Per Game";
+        break;
+      case 3:
+        return  "Field Goal Percentage";
+        break;
+      case 4:
+        return  "Three Pointers made";
+        break;
+      case 5:
+        return  "Blocks Per Game";
+        break;
+      case 6:
+        return  "Steals Per Game";
+    }
+
+  }
   return (
     <List
       sx={{
-        width: "100%",
-        height: 450,
-        bgcolor: "background.paper",
-        borderRadius:"0.5rem"
+        width: '100%',
+        bgcolor: 'background.paper',
+        position: 'relative',
+        overflow: 'auto',
+        maxHeight: 1115,
+        borderRadius: "0.5rem",
+        '& ul': { padding: 0 },
       }}
+      subheader={<li />}
     >
-      <ListItem>
-        <ListItemText secondary="Bulls derozen hits 2 buzzer beater to put Chicago on top" />
-      </ListItem>
+      {[0, 1, 2, 3, 4 ,5 ,6].map((sectionId) => (
+        <li key={`section-${sectionId}`}>
+          <ul>
+            <ListSubheader  style={{color:"black"}} >{`${getHeading(sectionId)}`}</ListSubheader>
+            {[0, 1, 2].map((item) => (
+              <ListItem key={`item-${sectionId}-${item}`}>
+                <ListItemText style={{color:"grey"}}  primary={`Item ${item}`} />
+              </ListItem>
+            ))}
+          </ul>
+        </li>
+      ))}
     </List>
   );
 }
+
