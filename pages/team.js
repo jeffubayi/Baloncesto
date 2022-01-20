c64c62661d3a77d20c0c352d72d2d17b9f6e683d
jeffubayi <jeffubayi@gmail.com>
2022-01-20 13:55:08 +0300

    update teams responsively

--- pages/team.js
+++ pages/team.js
@@ -1,18 +1,19 @@
-// 
+//
 import * as React from "react";
 import List from "@mui/material/List";
 import Box from "@mui/material/Box";
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import ListSubheader from '@mui/material/ListSubheader';
-import IconButton from '@mui/material/IconButton';
-import InfoIcon from '@mui/icons-material/Info';
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import ListSubheader from "@mui/material/ListSubheader";
+import IconButton from "@mui/material/IconButton";
+import InfoIcon from "@mui/icons-material/Info";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export default function TitlebarImageList() {
   const [teams, setTeams] = React.useState([]);
   const axios = require("axios");
-
+  const isSmallWindow = useMediaQuery(`(max-width:768px)`);
   async function getTeams() {
     axios
       .get("https://data.nba.net/prod/v1/2021/teams.json")
@@ -22,36 +23,86 @@ export default function TitlebarImageList() {
         setTeams(teamArray);
       });
   }
-  
+
   React.useEffect(() => getTeams(), []);
 
   return (
-    <ImageList sx={{ width:"auto", height: 800 }}>
-      <ImageListItem key="Subheader" cols={6}>
-        <ListSubheader component="p" sx={{ textAlign:"center",color:"black" }}>All NBA Franchise Teams</ListSubheader>
-      </ImageListItem>
-      {teams.sacramento ? teams.sacramento.map((item) => (
-        <ImageListItem key={item.teamId}>
-          <img
-            src={`/${item.tricode}.png`}
-            srcSet={`/${item.tricode}.png`}
-            alt={item.fullName}
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title={item.fullName}
-            subtitle={item.confName}
-            actionIcon={
-              <IconButton
-                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                aria-label={`info about ${item.fullName}`}
-              >
-                <InfoIcon />
-              </IconButton>
-            }
-          />
-        </ImageListItem>
-     )): <p>Not Available</p>}
-    </ImageList>
+    <>
+      {isSmallWindow ? (
+        <ImageList sx={{ width: "100%", height: "auto" }}>
+          <ImageListItem key="Subheader" cols={2}>
+            <ListSubheader
+              component="p"
+              sx={{ textAlign: "center", color: "black" }}
+            >
+              All NBA Franchise Teams
+            </ListSubheader>
+          </ImageListItem>
+          {teams.sacramento ? (
+            teams.sacramento.filter(sac => !sac.tricode.includes("UTW")).map((item) => (
+              <ImageListItem key={item.teamId}>
+                <img
+                  src={`/${item.tricode}.png`}
+                  srcSet={`/${item.tricode}.png`}
+                  alt={item.fullName}
+                  loading="lazy"
+                />
+                <ImageListItemBar
+                  title={item.fullName}
+                  subtitle={item.confName}
+                  actionIcon={
+                    <IconButton
+                      sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                      aria-label={`info about ${item.fullName}`}
+                    >
+                      <InfoIcon />
+                    </IconButton>
+                  }
+                />
+              </ImageListItem>
+            ))
+          ) : (
+            <p>Not Available</p>
+          )}
+        </ImageList>
+      ) : (
+        <ImageList sx={{ width: "100%", height: 1000 }}>
+          <ImageListItem key="Subheader" cols={6}>
+            <ListSubheader
+              component="p"
+              sx={{ textAlign: "center", color: "black" }}
+            >
+              All NBA Franchise Teams
+            </ListSubheader>
+          </ImageListItem>
+          {teams.sacramento ? (
+            teams.sacramento.map((item) => (
+              <ImageListItem key={item.teamId}>
+                <img
+                  src={`/${item.tricode}.png`}
+                  srcSet={`/${item.tricode}.png`}
+                  alt={item.fullName}
+                  loading="lazy"
+                />
+                <ImageListItemBar
+                  title={item.fullName}
+                  subtitle={item.confName}
+                  actionIcon={
+                    <IconButton
+                      sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                      aria-label={`info about ${item.fullName}`}
+                    >
+                      <InfoIcon />
+                    </IconButton>
+                  }
+                />
+              </ImageListItem>
+            ))
+          ) : (
+            <p>Not Available</p>
+          )}
+        </ImageList>
+      )}
+    </>
   );
 }
