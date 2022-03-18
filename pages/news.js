fe3c6b30166f9e2b14a6c3eb7d534576b243d584
jeffubayi <jeffubayi@gmail.com>
2022-03-18 21:39:40 +0300

    add news page

---
+++ pages/news.js
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import { Button, Card } from "@mui/material";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { StyledDiv } from "./index";
+import Link from "next/link";
+
+export default function NewsCard({ newsArray: newsArray }) {
+  return (
+    <>
+      <StyledDiv>
+        {newsArray.length > 0 &&
+          newsArray.map((news) => (
+            <Card sx={{ borderRadius: "0.3rem" }} elevation={0}>
+              <CardMedia
+                component="img"
+                height={350}
+                image={news.media}
+                alt="nba"
+              />
+              <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                  {news.title}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Link href={news.link}>
+                  <Button size="small"  sx={{ textTransform: "capitalize",color: "#051c2d" }}>
+                    Read
+                  </Button>
+                </Link>
+              </CardActions>
+            </Card>
+          ))}
+      </StyledDiv>
+    </>
+  );
+}
